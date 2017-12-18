6150520ab47ba22a45cc8bdb2ceba9c61122a1a6
eng.mhd.bitar <mohammad.bitar@das-360.com>
2017-12-18 14:17:31 +0300

    Finish the simple dashboard

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,18 @@
+<template>
+	<footer class="footer">
+	  <div class="container">
+	    <div class="content has-text-centered">
+	      <p>
+	        <strong>Vuejs Phonebook App</strong> by <a href="http://jgthms.com">Mohammad Bitar</a>. Access the source code
+	        <a href="https://github.com/mhdbitar/phonebook">Phonebook</a>.
+	      </p>
+
+	      <p>
+	      	<a href="https://github.com/mhdbitar/phonebook" class="icon">
+	      		<i class="fa fa-github"></i>
+	      	</a>
+	      </p>
+	    </div>
+	  </div>
+	</footer>
+</template>
---
+++ resources/assets/js/components/Myheader.vue
@@ -0,0 +1,20 @@
+<template>
+	<nav class="navbar" role="navigation" aria-label="main navigation">
+	  <div class="navbar-brand">
+	  	<router-link to="/home" class="navbar-item">
+	  		<img src="https://bulma.io/images/bulma-logo.png" alt='Mohammad' width="112" height="28">		    	
+	    	</router-link>
+
+	<div class="navbar-menu is-active">
+		<router-link class="navbar-item" to="/home">Home</router-link>
+    		<router-link class="navbar-item" to="/about">About</router-link>		
+	</div>
+	    
+	    <button class="button navbar-burger">
+	      <span></span>
+	      <span></span>
+	      <span></span>
+	    </button>
+	  </div>
+	</nav>
+</template>
